8001f66a5ffbc81ea4587f94ee471126056c21ff
Fabrice Gabolde <fabrice.gabolde@uperto.com>
2011-11-14 19:01:48 +0100

    POD for the main Shell class.

--- lib/Zabbix/API/Shell.pm
+++ lib/Zabbix/API/Shell.pm
@@ -563,6 +563,227 @@ Zabbix::API::Shell -- Command line shell to interact with a Zabbix instance
 This is a class derived from Term::Shell.  Running its instance method
 C<cmdloop> brings up a console client for Zabbix servers.
 
+=head1 METHODS
+
+All of these C<run_foo> methods implement the corresponding C<foo> command.  The
+methods specific to each object (e.g. a Host's C<items> command) are implemented
+by C<catch_run>, in an ugly catch-all eval-if block.
+
+The existence of a C<Zabbix::API::Unicorn> class, mapping a (sadly,
+hypothetical) unicorn-like object in the remote API, is assumed.  This class is
+mentioned wherever the actual type does not matter.
+
+=over 4
+
+=item run_config
+
+This command manipulates zapishell's configuration interactively.  There are
+several subcommands for the C<config> command:
+
+=over 4
+
+=item set
+
+  $ config set foo bar
+
+Set the config value C<foo> to C<bar>.
+
+=item unset
+
+  $ config unset foo
+
+Delete the config value C<foo>.
+
+=item dump
+
+  $ config dump
+
+Dump the current configuration to screen, in YAML format.
+
+  $ config dump foo
+
+Dump the value of C<foo> in the current configuration.
+
+=item commit
+
+Both of these require interactive confirmation by the user.
+
+  $ config commit
+
+Writes the current configuration to file.  If you do not do this, your
+modifications will be forgotten once you exit C<zapishell>!
+
+  $ config commit foo
+
+Writes the value of C<foo> in the current configuration to the config file.
+
+=back
+
+=item run_connect
+
+  $ connect http://127.0.0.1/zabbix/api_jsonrpc.php username password
+
+Creates a Zabbix::API object connected to the URL given as the first argument,
+logs in as C<username> with password C<password>, and displays the remote API's
+version.
+
+All other commands (except C<config>) will complain if the user has not
+connected.
+
+=item run_select
+
+  $ select Unicorn 10018
+
+Select a stashed item by type and ID. (see C<run_fetch> for an explanation of
+the stash).  The user can now run commands without needing to specify the object
+every time, and the selected object's own commands (e.g. C<items> for Host type
+objects) become available.
+
+This means that in the documentation of other commands which require an object's
+type and ID, you can remove those two parameters:
+
+  $ examine Unicorn 10018
+
+becomes
+
+  $ examine
+
+=item run_fetch
+
+  $ fetch Unicorn id 10018
+
+Fetches (pulls) an item from the server, by type and arbitrary parameters.  The
+parameters are given as a hashref to the type's search parameter.
+
+The example above results in something like
+
+  $ $zabbix->fetch('Unicorn', params => { search => { id => 10018 }});
+
+The item(s) fetched are then stashed, which means they can be used by the other
+commands if referred to by type and ID, and they can be selected.
+
+You can't use C<select> to indicate an object to be fetched, but you can use the
+object's own C<pull> method instead (that's probably what you were looking for,
+right?).
+
+=item run_examine
+
+  $ examine Unicorn 10018
+
+Dumps the object's data to screen, in YAML format.
+
+=item run_filter
+
+  $ filter Unicorn color white
+
+Prints the list of IDs of Unicorns whose color smart-matches "white".  For this
+to work, C<color> needs to be a member of the object's data.  For now this only
+does a very simple direct match; we could later expand this to regexes,
+parent-child relationships ("hosts that have items like system.uptime"), etc.
+
+Later on this might become C<list Unicorn color white>, since C<list> does not
+take any useful arguments beyond a type.
+
+=item run_list
+
+  $ list Unicorn
+
+Prints the list of IDs of stashed objects whose type is given as parameter.  If
+you can't see an object and you're pretty sure it's on the server, you probably
+haven't C<fetch>ed it.
+
+=item run_set
+
+  # turn it into a cross-aligned unicorn
+  $ set Unicorn 10018 color black
+
+Sets the object's attribute to a specific value.  There are several caveats
+associated with this command, the most important being "It may not work."
+
+=over 4
+
+=item
+
+It doesn't understand C<select>ed objects, because of the very primitive way we
+handle parameters.
+
+=item
+
+It doesn't understand complex data structures, because we have no elegant way of
+specifying them on the command line (maybe JSON-encoded).
+
+=item
+
+The user needs to remember to C<push> the object afterwards, and object methods
+are still not entirely reliable.
+
+=back
+
+=item catch_run
+
+This method attempts to make sense of nonsense commands by assuming they're
+methods of the currently C<select>ed object and YAML-dumping the result to
+screen, so for instance this works:
+
+  $ select Host 10024
+  $ items
+
+Since a YAML dump of the actual C<Zabbix::API::Item> objects would be huge, it's
+reduced to a representative form of each element in the resulting structure,
+which in this case is a list of name => NAME, id => ID hashes.  It's a naive,
+recursive data walk with no state, so dumping the contents of recursive data
+structures may not be the best idea (it loops forever).
+
+=back
+
+=head2 FOR DEVELOPERS ONLY
+
+If you are not interested in hacking C<zapishell>, only in using it, then you're
+probably not interested in this section either.
+
+=over 4
+
+=item get_object
+
+Multipurpose helper to return the currently selected object, or the object
+specified in the command:
+
+  # $zapishell->get_object('Unicorn', 10018) under the hood
+  $ examine Unicorn 10018
+
+  $ select Unicorn 10018
+  # same thing
+  $ examine
+
+=item preloop
+
+Displays the banner and sets up a few internals: configuration, readline-related
+things (prompt, history), default pager (better hope you have /usr/bin/less --
+don't worry, this will change in a future release).
+
+=item postloop
+
+Saves the readline history if the underlying implementation understands it.
+
+=item is_connected
+
+Returns a true value if the shell is connected to a remote API.
+
+=item prompt_str
+
+Builds an appropriate prompt string.  It looks like this:
+
+  # not connected
+  $ nobody@nowhere:/$ 
+
+  # connected
+  $ username@hostname:/$ 
+
+  # connected and object selected
+  $ username@hostname:[unicorn IPU]$ 
+
+=back
+
 =head1 SEE ALSO
 
 L<zapishell>.
